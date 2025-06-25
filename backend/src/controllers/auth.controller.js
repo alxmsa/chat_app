9b0b46466bb2bf560fd16592649ed6c17193df66
Mouss <moussapro.dp@gmail.com>
2025-06-25 10:45:28 +0200

    backend : ajout dossiers controllers et routes

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export async function signup(req, res) {
+    res.send("Signup Route");
+}
+export async function login(req, res) {
+    res.send("Login Route");
+}
+export function Logout(req, res) {
+    res.send("Logout Route");
+}
---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { login, Logout, signup } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", Logout);
+
+export default router;
