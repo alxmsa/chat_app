90c6906e6afcb3219e719ffec6802141c129f557
Mouss <moussapro.dp@gmail.com>
2025-06-30 15:46:12 +0200

    acceptFriendRequest done ✅

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -70,3 +70,38 @@ export async function sendFriendRequest(req,res) {
         res.status(500).json({ message: "Internal Server Error"});
     }
 }
+
+export async function acceptFriendRequest (req, res) {
+    try {
+        const {id:requestId} = req.params;
+
+        const friendRequest = await FriendRequest.findById(requestId);
+
+        if (!friendRequest) {
+            return res.status(404).json({ message: "Friend request not found"});
+        }
+
+        if (friendRequest.recipient.toString() !== req.user.id) {
+            return res. status(403).json({ message: "You are not auhtorized to accept this request"});
+        }
+
+        friendRequest.status= "accepted";
+        await friendRequest.save();
+
+        // add each user s arrato the other's friendy
+        // $addToSet: adds element to an array only if they do not already exist.
+        await User.findByIdAndUpdate(friendRequest.sender, {
+            $addToSet: { friends: friendRequest.recipient},
+        });
+        
+        await User.findByIdAndUpdate(friendRequest.recipient, {
+            $addToSet: { friends: friendRequest.sender},
+        });
+
+        res.status(200).json({message: "Friend request accepted"});
+
+    } catch (error) {
+        console.log("Error in acceptFriendRequest controller", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
+}
